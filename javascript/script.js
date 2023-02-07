a088c127afea726fe38928d420f420e1fdc418ec
Vi <m2107313@middlesbro.ac.uk>
2023-02-07 04:50:15 +0000

    change javascript file

--- javascript/script.js
+++ javascript/script.js
@@ -16,3 +16,23 @@ function searchCalories (event) {
     console.log(data)
 })
 }
+
+
+/* Search bar for API-Ninjas-Calories-Burned */
+
+const searchBtn = document.getElementById("search-btn")
+
+searchBtn.addEventListener("click",searchCalories)
+
+function searchCalories (event) {
+    const queryString = "https://api.api-ninjas.com/v1/caloriesburned?activity=" + document.getElementById("search-bar").value
+    event.preventDefault();
+    console.log(queryString)
+    fetch(queryString,{method:"GET",headers:{
+    'X-Api-Key':"FUTTDF8tfgQ2Kp0ARtHAmw==L68CvqvDQpMYdgnE"
+}}).then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data)
+})
+}
