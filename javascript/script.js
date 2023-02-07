12fc480bff4e8f0d7bdb093f3a724c2ed925ced3
Vi <m2107313@middlesbro.ac.uk>
2023-02-07 04:42:26 +0000

    change index.js index.html and css file

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+/* Search bar for API-Ninjas-Calories-Burned */
+
+const searchBtn = document.getElementById("search-btn")
+
+searchBtn.addEventListener("click",searchCalories)
+
+function searchCalories (event) {
+    const queryString = "https://api.api-ninjas.com/v1/caloriesburned?activity=" + document.getElementById("search-bar").value
+    event.preventDefault();
+    console.log(queryString)
+    fetch(queryString,{method:"GET",headers:{
+    'X-Api-Key':"FUTTDF8tfgQ2Kp0ARtHAmw==L68CvqvDQpMYdgnE"
+}}).then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data)
+})
+}
